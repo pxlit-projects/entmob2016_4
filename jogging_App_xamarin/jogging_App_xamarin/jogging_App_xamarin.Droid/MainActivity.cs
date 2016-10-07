8262cfc8ade760afa88eec97e8349c81a0314065
stefanie <ramaekersstefanie@hotmail.com>
2016-10-07 13:49:53 +0200

    adding XamarinApp empty

---
+++ jogging_App_xamarin/jogging_App_xamarin/jogging_App_xamarin.Droid/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace jogging_App_xamarin.Droid
+{
+    [Activity(Label = "jogging_App_xamarin", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+           /* TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            */
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+           // Xamarin.FormsMaps.init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
