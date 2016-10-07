ade9f618d1d7bcf82b3c290b64004b40865506f7
jonasbeckers <Jonas@MacBook-Pro-van-Jonas-2.local>
2016-10-07 09:01:21 +0200

    initial

--- src/Temperature.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Allardke on 23/09/2016.
- */
-public class Temperature {
-}
---
+++ src/main/java/Models/Humidity.java
@@ -0,0 +1,7 @@
+package Models;
+
+/**
+ * Created by Jonas on 7/10/16.
+ */
+public class Humidity {
+}
