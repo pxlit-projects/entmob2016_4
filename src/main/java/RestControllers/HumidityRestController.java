941f41e833a95207edd6d3ca1299be6c32a3087a
jonasbeckers <Jonas@MacBook-Pro-van-Jonas-2.local>
2016-10-07 10:26:35 +0200

    added restcontrollers

---
+++ src/main/java/RestControllers/HumidityRestController.java
@@ -0,0 +1,26 @@
+package RestControllers;
+
+import Models.Humidity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Jonas on 7/10/16.
+ */
+@RestController
+@RequestMapping("/humdity")
+public class HumidityRestController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Humidity> getHumidityRange(@RequestParam(value = "amount") int amount) {
+        return new ArrayList<Humidity>();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Humidity addHumidity(@RequestBody Humidity humidity) {
+        return new Humidity();
+    }
+
+}
