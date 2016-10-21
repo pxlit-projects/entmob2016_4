35c85fd4f79361f88416b8d987e3666946c8c2e5
jonasbeckers <jonas.beckers1996@gmail.com>
2016-10-21 16:28:53 +0200

    intial

---
+++ EntMob_Xamarin/EntMob_Xamarin/EntMob_Xamarin/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EntMob_Xamarin;
+
+using Xamarin.Forms;
+
+namespace EntMob_Xamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ EntMob_Xamarin/EntMob_Xamarin/EntMob_Xamarin/TimerPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntMob_Xamarin.ViewModels;
+
+
+
+using Xamarin.Forms;
+using Java.Util;
+
+namespace EntMob_Xamarin
+{
+    public partial class TimerPage : ContentPage
+    {
+        TimerViewModel _TimerViewModel;
+
+        public TimerPage()
+        {
+            InitializeComponent();
+            _TimerViewModel = new TimerViewModel();
+            BindingContext = _TimerViewModel;
+          
+
+        }
+
+        public void ItemClicked(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            if (button.Text == "Stop")
+            {
+                button.Text = "Start";
+                Navigation.PushAsync(new ValuesPage());
+
+            }else
+            {
+                _TimerViewModel.StartTimer();
+                button.Text = "Stop";
+            }
+
+        }
+       
+
+    }
+}
