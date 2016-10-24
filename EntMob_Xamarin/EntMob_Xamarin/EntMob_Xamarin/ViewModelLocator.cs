d08362ccf99e7c2a0fcf8ec53fe0fed5efde93d1
allardjonas <11306641@student.pxl.be>
2016-10-24 15:59:52 +0200

    viewmodellocator

---
+++ EntMob_Xamarin/EntMob_Xamarin/EntMob_Xamarin/ViewModelLocator.cs
@@ -0,0 +1,45 @@
+﻿using EntMob_Xamarin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntMob_Xamarin
+{
+    public class ViewModelLocator
+    {
+        private static RunnerViewModel runnerViewModel;
+        private static TimerViewModel timerViewModel;
+        private static ValuesViewModel valuesViewModel;
+
+        //private static IDialogService dialogService = new DialogService(parkingLotSelectionViewModel.SelectedParking, routeDetailViewModel.selectedRoute);
+        //private static IParkingLotDataService parkingLotDataService = new ParkingLotDataService(new ParkingsRepository());
+
+        public static RunnerViewModel RunnerViewModel
+        {
+            get
+            {
+                return runnerViewModel ?? (runnerViewModel = new RunnerViewModel(/*parkingLotDataService, dialogService*/));
+            }
+        }
+
+
+        public static TimerViewModel DetailViewModel
+        {
+            get
+            {
+                return timerViewModel ?? (timerViewModel = new TimerViewModel(/*parkingLotDataService, dialogService*/));
+            }
+        }
+
+
+        public static ValuesViewModel ValuesViewModel
+        {
+            get
+            {
+                return valuesViewModel ?? (valuesViewModel = new ValuesViewModel(/*parkingLotDataService, dialogService*/));
+            }
+        }
+    }
+}
