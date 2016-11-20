8241f5fd41e9738f8a10be995d1f7182c9b8797f
jonasbeckers <jonas.beckers1996@gmail.com>
2016-11-20 14:35:55 +0100

    added mocks

---
+++ EntMob_Uni/Jogging.Test/Mocks/MockUserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Jogging.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jogging.Model;
+
+namespace Jogging.Test.Mocks
+{
+    class MockUserRepository : IUserRepository
+    {
+        public Task<User> GetUserByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> PostUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- EntMob_Uni/Jogging.Test/ValuesViewModelTest.cs
+++ EntMob_Uni/Jogging.Test/ValuesViewModelTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+﻿using EntMob_Uni.Services;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +15,7 @@ namespace Jogging.Test
         [TestMethod]
         public void Test()
         {
-            
+            IUserService userService = new MockUserService
         }
 
     }
